dd61672d7fc7a0d08159e9997d27cc4116aa87b1
Hoturan <davidwcorral@hotmail.com>
2018-04-23 16:33:41 +0200

    Added models_grans

--- practica2/algorithm/RGBtest.m
+++ practica2/algorithm/RGBtest.m
@@ -3,6 +3,6 @@ hist = imhist(im(:,:,1))
 nbins = 20;
 
 nelem = numel(im)/3;
-[freq, freq_emph, freq_ly] = image_hist_RGB_3d('../models/acmilan/35.jpg',6);
+hols = image_hist_RGB_3d('../models/acmilan/35.jpg',6);
 
 
--- practica2/algorithm/test_score.m
+++ practica2/algorithm/test_score.m
@@ -1,47 +1,57 @@
 %when debug is true, the test outputs the result for every image
 debug = false;
 
+models = '../models/';
+models_grans = '../models_grans/';
+
+model_used = models_grans;
+
 global acmilan1 acmilan2 acmilan3
-acmilan1 = imread('../models/acmilan/35.jpg');
-acmilan2 = imread('../models/acmilan/07.jpg');
-acmilan3 = imread('../models/acmilan/11.jpg');
+acmilan1 = imread(strcat(model_used, 'acmilan/35.jpg'));
+acmilan2 = imread(strcat(model_used,'acmilan/07.jpg'));
+acmilan3 = imread(strcat(model_used, 'acmilan/11.jpg'));
 
 global barcelona1 barcelona2 barcelona3
-barcelona1 = imread('../models/barcelona/04.jpg');
-barcelona2 = imread('../models/barcelona/20.jpg');
-barcelona3 = imread('../models/barcelona/29.jpg');
+barcelona1 = imread(strcat(model_used, 'barcelona/04.jpg'));
+barcelona2 = imread(strcat(model_used, 'barcelona/20.jpg'));
+barcelona3 = imread(strcat(model_used, 'barcelona/29.jpg'));
 
 global chelsea1 chelsea2 chelsea3
-chelsea1 = imread('../models/chelsea/16.jpg');
-chelsea2 = imread('../models/chelsea/10.jpg');
-chelsea3 = imread('../models/chelsea/18.jpg');
+chelsea1 = imread(strcat(model_used,'chelsea/16.jpg'));
+chelsea2 = imread(strcat(model_used, 'chelsea/10.jpg'));
+chelsea3 = imread(strcat(model_used, 'chelsea/18.jpg'));
 
 global juventus1 juventus2 juventus3
-juventus1 = imread('../models/juventus/27.jpg');
-juventus2 = imread('../models/juventus/09.jpg');
-juventus3 = imread('../models/juventus/36.jpg');
+juventus1 = imread(strcat(model_used, 'juventus/27.jpg'));
+juventus2 = imread(strcat(model_used, 'juventus/09.jpg'));
+juventus3 = imread(strcat(model_used, 'juventus/36.jpg'));
 
 
 global liverpool1 liverpool2 liverpool3
-liverpool1 = imread('../models/liverpool/25.jpg');
-liverpool2 = imread('../models/liverpool/11.jpg');
-liverpool3 = imread('../models/liverpool/24.jpg');
+liverpool1 = imread(strcat(model_used, 'liverpool/25.jpg'));
+liverpool2 = imread(strcat(model_used, 'liverpool/11.jpg'));
+liverpool3 = imread(strcat(model_used, 'liverpool/24.jpg'));
 
 
 global madrid1 madrid2 madrid3
-madrid1 = imread('../models/madrid/31.jpg');
-madrid2 = imread('../models/madrid/17.jpg');
-madrid3 = imread('../models/madrid/35.jpg');
+madrid1 = imread(strcat(model_used, 'madrid/31.jpg'));
+madrid2 = imread(strcat(model_used, 'madrid/17.jpg'));
+madrid3 = imread(strcat(model_used, 'madrid/35.jpg'));
 
 
 global psv1 psv2 psv3
-psv1 = imread('../models/psv/31.jpg');
-psv2 = imread('../models/psv/17.jpg');
-psv3 = imread('../models/psv/39.jpg');
+psv1 = imread(strcat(model_used, 'psv/31.jpg'));
+psv2 = imread(strcat(model_used, 'psv/17.jpg'));
+psv3 = imread(strcat(model_used, 'psv/39.jpg'));
 
 
 teams = dir('../soccer');
 teams(1:2) = [];
+
+if strcmp(teams(1).name, '.DS_Store')
+    teams(1) = [];
+end
+
 for team = teams'
     fprintf('analyzing %s... ',team.name);
     images = dir(['../soccer/',team.name]);
