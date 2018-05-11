8fd1e2ac78c7a3724d7b85646564f08af890d12e
Unknown <davidwcorral@hotmail.com>
2018-05-11 13:36:04 +0200

    small main not fully working

---
+++ practica3/alogrithm/main.m
@@ -0,0 +1,32 @@
+%when debug is true, the test outputs the result for every image
+debug = false;
+
+
+animals = dir('../animals');
+animals(1:2) = [];
+
+if strcmp(animals(1).name, '.DS_Store')
+    animals(1) = [];
+end
+
+keySet = {'ant', 'beaver', 'crab', 'crayfish', 'crocodile', 'dolphin', 'dragonfly', 'elephant', 'emu', 'flamingo', 'kangaroo', 'panda'};
+valueSet = {[[]] [[]] [[]] [[]] [[]] [[]] [[]] [[]] [[]] [[]] [[]] [[]]};
+
+annMap = containers.Map(keySet,valueSet);
+
+for animals = animals'
+    files = dir(['../animals/',animals.name]);
+    files(1:2) = [];
+    images = files;
+   
+    for k = 1:length(files)
+        baseFileName = files(k).name;
+        [filepath,name,ext] = fileparts(['../animals/',animals.name,'/',baseFileName]);
+        if ext == ".mat"
+            annMap(animals.name) = [annMap(animals.name) ['../animals/',animals.name,'/',baseFileName]];
+        else
+            break
+        end
+    end
+    
+end
