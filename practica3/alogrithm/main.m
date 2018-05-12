437bdaf593debcee0d909f327607f8a185e6ba4c
Hoturan <davidwcorral@hotmail.com>
2018-05-12 15:02:12 +0200

    Train_model and modified main

--- practica3/alogrithm/main.m
+++ practica3/alogrithm/main.m
@@ -9,24 +9,32 @@ if strcmp(animals(1).name, '.DS_Store')
     animals(1) = [];
 end
 
-keySet = {'ant', 'beaver', 'crab', 'crayfish', 'crocodile', 'dolphin', 'dragonfly', 'elephant', 'emu', 'flamingo', 'kangaroo', 'panda'};
-valueSet = {[] [] [] [] [] [] [] [] [] [] [] []};
+%keySet = {'ant', 'beaver', 'crab', 'crayfish', 'crocodile', 'dolphin', 'dragonfly', 'elephant', 'emu', 'flamingo', 'kangaroo', 'panda'};
+%valueSet = {[] [] [] [] [] [] [] [] [] [] [] []};
 
-annMap = containers.Map(keySet,valueSet, 'UniformValues',false);
+%annMap = containers.Map(keySet,valueSet, 'UniformValues',false);
+
+categoryClassifier = train_model;
 
 for animals = animals'
     files = dir(['../animals/',animals.name]);
     files(1:2) = [];
     images = files;
-   
+    countCorrect = 0;
     for k = 1:length(files)
         baseFileName = files(k).name;
         [filepath,name,ext] = fileparts(['../animals/',animals.name,'/',baseFileName]);
-        if ext == ".mat"
-            annMap(animals.name) = vertcat(annMap(animals.name), convertCharsToStrings(['../animals/',animals.name,'/',baseFileName]));
-        else
-            break
+        %display(ext)
+        %if ext == ".mat"
+        %    annMap(animals.name) = vertcat(annMap(animals.name), convertCharsToStrings(['../animals/',animals.name,'/',baseFileName]));
+        if ext == ".jpg"
+            img = imread(['../animals/',animals.name,'/',baseFileName]);
+            [labelIdx, scores] = predict(categoryClassifier, img);
+            predictedAnimal = categoryClassifier.Labels(labelIdx);
+            if (string(predictedAnimal) == animals.name)
+                countCorrect = countCorrect + 1;
+            end
         end
     end
-    
+    fprintf(' model detects %.2f %% of %s images.\n',countCorrect/(length(files)/2)*100,animals.name);
 end
---
+++ practica3/alogrithm/train_model.m
@@ -0,0 +1,31 @@
+function [categoryClassifier] = train_model
+categories = {'ant', 'beaver', 'crab', 'crayfish', 'crocodile', 'dolphin', 'dragonfly', 'elephant', 'emu', 'flamingo', 'kangaroo', 'panda'};
+imds = imageDatastore(fullfile('../animals', categories), 'LabelSource', 'foldernames');
+tbl = countEachLabel(imds)
+minSetCount = min(tbl{:,2}); % determine the smallest amount of images in a category
+
+% Use splitEachLabel method to trim the set.
+imds = splitEachLabel(imds, minSetCount, 'randomize');
+
+% Notice that each set now has exactly the same number of images.
+countEachLabel(imds)
+
+[trainingSet, validationSet] = splitEachLabel(imds, 0.2, 'randomize');
+
+bag = bagOfFeatures(trainingSet);
+
+img = readimage(imds, 1);
+featureVector = encode(bag, img);
+
+% Plot the histogram of visual word occurrences
+figure
+bar(featureVector)
+title('Visual word occurrences')
+xlabel('Visual word index')
+ylabel('Frequency of occurrence')
+
+categoryClassifier = trainImageCategoryClassifier(trainingSet, bag);
+confMatrix = evaluate(categoryClassifier, trainingSet);
+
+mean(diag(confMatrix));
+end
