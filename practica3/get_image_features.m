d3cca70bb9e2397e2f86f4b391ce3d9ef51ccace
arnaubadia <arnaubadia96@gmail.com>
2018-05-15 18:31:18 +0200

    minor refactoring

--- practica3/get_image_features.m
+++ practica3/get_image_features.m
@@ -1,4 +1,4 @@
-function x = get_image_features(image,box_coord,contour)
+function x = get_image_features(img,box_coord,contour)
  %figure, imshow(image)
 %for a certain image, we must extract features
 %this functions returns an array of features
@@ -6,14 +6,52 @@ function x = get_image_features(image,box_coord,contour)
     bb_hmax = box_coord(2);
     bb_wmin = box_coord(3);
     bb_wmax = box_coord(4);
-    
     c_x = contour(1,:);
     c_y = contour(2,:);
     
+    %generate bw image
+    bb_img = img(bb_hmin:bb_hmax,bb_wmin:bb_wmax);
+    bw_img = roipoly(bb_img,c_x,c_y);
     
+    %BOUNDING BOX AREA
     bb_area = (bb_hmax-bb_hmin)*(bb_wmax-bb_wmin);
-    animal_area = polyarea(c_x,c_y);
+    %ANIMAL AREA (bwarea returns area of white pixels)
+    animal_area = bwarea(bw_img);
+
     
+
+    
+    %feature 1: ratio between bb area and animal area
+
+    ratio_animal_area_to_bb = animal_area / bb_area;
+       
+    
+    %feature 2: regionprops 
+    %regProps = regionprops(binaryIm);
+
+    %feature 3: area of legs
+    ee = strel('disk',30);
+    th = imopen(bw_img,ee);
+    im = bw_img-th;
+    im = bwareaopen(im,300);
+    areaLegs = regionprops(im);
+    areaLegs = areaLegs.Area;
+    
+    
+    %......
+    
+    
+    %return array of features (now it has only one feature
+    x = [ratio_animal_area_to_bb, areaLegs];
+
+
+end
+
+
+
+
+
+
     % Turn contour and box into a filled binary image
     %{
     blackRectangle = zeros(bb_hmax-bb_hmin, bb_wmax-bb_wmin);
@@ -34,33 +72,4 @@ function x = get_image_features(image,box_coord,contour)
     binaryIm = imfill(BW,'holes'); %ERROR HERE WITH IMAGE image_0010.jpg
     figure, imshow(binaryIm)
     %figure, imshow(binaryIm)
-    
     %}
-    
-    bb_img = image(box_coord(1):box_coord(2),box_coord(3):box_coord(4));
-    bw_img = roipoly(bb_img,c_x,c_y);
-    
-    %feature 1: ratio between bb area and animal area
-    ratio_animal_area_to_bb = animal_area / bb_area;
-    
-    
-    %feature 2: regionprops 
-    %regProps = regionprops(binaryIm);
-
-    %feature 3: area of legs
-%     ee = strel('disk',30);
-%     th = imopen(binaryIm,ee);
-%     im = binaryIm-th;
-%     im = bwareaopen(im,300);
-%     areaLegs = regionprops(im);
-%     areaLegs = areaLegs.Area;
-    
-    
-    %......
-    
-    
-    %return array of features (now it has only one feature
-    x = [ratio_animal_area_to_bb];
-
-
-end
